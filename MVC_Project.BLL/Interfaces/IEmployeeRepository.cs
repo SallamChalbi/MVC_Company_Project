7d3a091358c53322a9d57815a43404e3f2562a6b
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2026-03-30 13:41:00 +0100

    BLL - Employee Repository

---
+++ MVC_Project.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using MVC_Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee? GetById(int id);
+        int Add(Employee employee);
+        int Update(Employee employee);
+        int Delete(Employee employee);
+    }
+}
---
+++ MVC_Project.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,44 @@
+﻿using MVC_Project.BLL.Interfaces;
+using MVC_Project.DAL.Contexts;
+using MVC_Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly CompanyDbContext _dbContext;
+
+        public EmployeeRepository(CompanyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public int Add(Employee employee)
+        {
+            _dbContext.Employees.Add(employee);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Employee employee)
+        {
+            _dbContext.Employees.Remove(employee);
+            return _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<Employee> GetAll()
+            => _dbContext.Employees.ToList();
+
+        public Employee? GetById(int id)
+            => _dbContext.Employees.Find(id);
+
+        public int Update(Employee employee)
+        {
+            _dbContext.Employees.Update(employee);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
