3dded070d3ef2e041783e0ff4d442b4a0a10426f
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2026-03-30 12:04:45 +0100

    Update Department Identity Key

--- MVC_Project.DAL/Contexts/CompanyDbContext.cs
+++ MVC_Project.DAL/Contexts/CompanyDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MVC_Project.DAL.Configurations;
 using MVC_Project.DAL.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +14,14 @@ namespace MVC_Project.DAL.Contexts
     {
         public CompanyDbContext(DbContextOptions<CompanyDbContext> options): base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfiguration());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
         }
 
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
