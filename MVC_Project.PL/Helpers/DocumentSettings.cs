1c8c57a6a783b14be4ac218dd807d6d275d0aff9
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2026-04-09 17:53:03 +0100

    Document Settings - Upload File

---
+++ MVC_Project.PL/Helpers/DocumentSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace MVC_Project.PL.Helpers
+{
+    public class DocumentSettings
+    {
+        public static string UploadFile(IFormFile file, string folderName)
+        {
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\Files", folderName);
+            string fileName = $"{Guid.NewGuid()}-{file.FileName}";
+            string filePath = Path.Combine(folderPath, fileName);
+
+            using(var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+            return fileName;
+        }
+    }
+}
