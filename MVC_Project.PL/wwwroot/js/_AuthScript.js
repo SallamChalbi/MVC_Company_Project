de7990e95f0c04e0716526117ea6459e95ae6773
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2026-04-14 20:48:02 +0100

    Auth Layout & Account Controller (Action and View)

---
+++ MVC_Project.PL/wwwroot/js/_AuthScript.js
@@ -0,0 +1,14 @@
+﻿function togglePassword(inputId, element) {
+    var input = document.getElementById(inputId);
+    var icon = element.querySelector("i");
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash"); // 👈 invisible eye
+    } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
